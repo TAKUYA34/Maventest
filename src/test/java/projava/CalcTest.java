c9c582103b0c26226b9081c1da63ac220258a859
takuya-Y <hakenad907@gmail.com>
2024-11-15 08:10:56 +0900

    バグを検出するために、実装したソースコード

--- src/test/java/projava/CalcTest.java
+++ src/test/java/projava/CalcTest.java
@@ -7,5 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalcTest {
     @Test
       void add() {
+        assertEquals(6, new Calc().add(2, 4), "2 + 4 = 8");
+        // assertEquals(期待値, 実際の値()[テストを表すメッセージ])
     }
 }
