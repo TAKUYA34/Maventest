8f2f49519a94fa3df38b4af101881f3d91468bdb
takuya-Y <hakenad907@gmail.com>
2024-11-14 12:17:35 +0900

    ライブラリを参照し、アメリカの祝日一覧を表示するプログラム

--- src/main/java/projava/JapaneseHolidays2022.java
+++ src/main/java/projava/JapaneseHolidays2022.java
@@ -1,12 +1,16 @@
 package projava;
 
 import one.cafebabe.bc4j.BusinessCalendar;
+import one.cafebabe.bc4j.UnitedStates;
+
 import static one.cafebabe.bc4j.BusinessCalendar.JAPAN;
+import static one.cafebabe.bc4j.BusinessCalendar.UNITED_STATES;
+
 import java.time.LocalDate;
 
 public class JapaneseHolidays2022 {
     public static void main(String[] args) {
-        BusinessCalendar.newBuilder().holiday(JAPAN.PUBLIC_HOLIDAYS).build()
+        BusinessCalendar.newBuilder().holiday(UNITED_STATES.PUBLIC_HOLIDAYS).build()
                 .getHolidaysBetween(LocalDate.of(2022, 1, 1),
                         LocalDate.of(2022, 12, 31))
                 .forEach(System.out::println);
