67b2e35992ad185e2d56d23b7936a798e059ce46
takuya-Y <hakenad907@gmail.com>
2024-11-15 09:36:20 +0900

    JUnit 5 で追加されたアサーションメソッドの実装、テスト

--- src/test/java/projava/CalcTest.java
+++ src/test/java/projava/CalcTest.java
@@ -9,5 +9,16 @@ class CalcTest {
       void add() {
         assertEquals(6, new Calc().add(2, 4), "2 + 4 = 8");
         // assertEquals(期待値, 実際の値()[テストを表すメッセージ])
+
+        // 例外 NumberFormatException が投げられることを期待するテスト
+        assertThrows(NumberFormatException.class, () -> Integer.parseInt("￥10,000"), "￥や,が入っているのでパースできない");
+
+        // 例外が投げられないことを期待するテスト
+        assertDoesNotThrow(() -> new Calc().add(-100, 10), "負の値を渡しても例外はでない");
+
+        // 複数のアサーションメソッドを必ず実行するテスト
+        assertAll(() -> assertEquals(4, new Calc().add(2, 2), "2 + 2 = 4"),
+                () -> assertEquals(6, new Calc().add(2, 4), "2 + 4 = 6")
+        );
     }
 }
